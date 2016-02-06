3c92fb17707fb7f4eb0c351f7de9e82ba7d9a01a
Javelin89 <chris.beermann89@gmail.com>
2016-02-06 16:49:57 +0100

    Commit after intro, summary task missing

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,25 @@
 module Listable
-  # Listable methods go here
+  def format_description(description)
+    "#{description}".ljust(25)
+  end
+
+  def format_date(start_date, end_date = false)
+    dates = start_date.strftime("%D") if start_date
+    dates << " -- " + end_date.strftime("%D") if end_date
+    dates = "N/A" if !dates
+    return dates
+  end
+
+  def format_priority(priority)
+  	if priority == "high"
+  		value = " ⇧".colorize(:red)
+    elsif priority == "medium"
+    	value = " ⇨".colorize(:yellow)
+    elsif priority == "low"
+    	value = " ⇩".colorize(:green)
+    elsif !priority
+     	value = ""
+    end
+  end
+
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -5,15 +5,28 @@ class UdaciList
     @title = options[:title]
     @items = []
   end
+
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    if type == "todo"
+      @items.push TodoItem.new(description, options)
+    elsif type == "event"
+      @items.push EventItem.new(description, options)
+    elsif type == "link"
+      @items.push LinkItem.new(description, options)
+    else 
+      raise UdaciListErrors::InvalidItemError, "This is not a valid item type"
+    end
   end
+
   def delete(index)
-    @items.delete_at(index - 1)
+    if @items.length >= index
+      @items.delete_at(index - 1)
+    else
+      raise UdaciListErrors::IndexExceedsListSize, "This item does not exist"
+    end
   end
+
   def all
     puts "-" * @title.length
     puts @title
